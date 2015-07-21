758f08a1e71b1772ddddd95b9d317a5a70bbdaee
evilsunny <aline.abramova@gmail.com>
2015-07-22 01:37:56 +0300

    first mobile changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect'); 
 var minifyCss = require('gulp-minify-css');
-
+var autoprefixer = require('gulp-autoprefixer');
+  
 
 var path = {
 	sass: [
@@ -31,9 +32,25 @@ gulp.task('sass', function() {
     }))
     .pipe(connect.reload());
 });
-//Watch task
+
 gulp.task('watch',function() {
     gulp.watch(path.sass,['sass']);
 });
+ 
+gulp.task('minify-css', function() {
+  return gulp.src('./public/css/*.css')  
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
 
-gulp.task('default', ['sass', 'serve', 'watch']);
+gulp.task('autoprefixer'  , function () {
+    return gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', ['sass', 'serve', 'watch','minify-css','autoprefixer']);
